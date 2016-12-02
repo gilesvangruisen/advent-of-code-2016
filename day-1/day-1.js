103ec8a673bfd9d9b887617c35e3c16a81e8de68
Giles Van Gruisen <gilesvangruisen@me.com>
2016-12-02 11:39:27 -0500

    make it suck less

--- day-1/day-1.js
+++ day-1/day-1.js
@@ -1,86 +1,105 @@
 'use strict';
 
-var fs = require('fs')
+const fs = require('fs')
 
-const stepsText = fs.readFileSync('input.txt', 'utf8')
-const steps = stepsText.split(', ')
+const Direction = {
+  North: 0,
+  East: 1,
+  South: 2,
+  West: 3,
+}
 
-var santa = {
-  history: [{ x: 0, y: 0 }],
-  visitCounts: {},
-  position: function() {
-    return this.history[this.history.length - 1]
-  },
-  _direction: 0,
+const DirectionDelta = {
+  L: -1,
+  R: 1,
+}
 
-  direction: function() {
-    if (this._direction >= 0) {
-      return this._direction % 4
-    } else if ((this._direction % 2) !== 0) {
-      return Math.abs((this._direction - 2) % 4)
-    } else {
-      return Math.abs(this._direction % 4)
-    }
-  },
+const PositionDelta = {
+  North: { x: 0, y: 1 },
+  East: { x: 1, y: 0 },
+  South: { x: 0, y: -1 },
+  West: { x: -1, y: 0 },
+}
 
-  turn: function(direction) {
-    if (direction === 'R') {
-      this._direction = this._direction + 1
-    } else if (direction === 'L') {
-      this._direction = this._direction - 1
-    }
-  },
+const Santa = function () {
+  return {
+    history: [{ x: 0, y: 0 }],
+    direction: Direction.North,
 
-  takeStep: function() {
-    var nextPosition = this.position()
+    position: function() {
+      return this.history[this.history.length - 1]
+    },
 
-    switch(this.direction()) {
-      case 0:
-        nextPosition.y = nextPosition.y + 1
-        break;
-      case 1:
-        nextPosition.x = nextPosition.x + 1
-        break;
-      case 2:
-        nextPosition.y = nextPosition.y - 1
-        break;
-      case 3:
-        nextPosition.x = nextPosition.x - 1
-        break;
-    }
+    turn: function(direction) {
+      const newDirection = this.direction + DirectionDelta[direction]
 
-    this.recordHistory(nextPosition)
-  },
+      if (newDirection > 3) {
+        this.direction = 0
+      } else if (newDirection < 0) {
+        this.direction = 3
+      } else {
+        this.direction = newDirection
+      }
+    },
 
-  recordHistory: function() {
-    const key = JSON.stringify(this.position())
-    const visitCount = this.visitCounts[key]
+    moveForward: function() {
+      const direction = Object.keys(Direction)[this.direction]
+      const delta = PositionDelta[direction]
 
+      const newPosition = {
+        x: this.position().x + delta.x,
+        y: this.position().y + delta.y,
+      }
 
-    this.visitCounts[key] = (visitCount || 0) + 1
+      this.history.push(newPosition)
+    },
 
-    if (visitCount >= 1) {
-      console.log('FOUND REVISIT ', this.position())
-    }
-  },
+    revisits: function() {
+      const visitCounts = this.history
+        .reduce((accum, position) => {
+          const key = JSON.stringify(position)
+          accum[key] = (accum[key] || 0) + 1
 
-  takeSteps: function(steps) {
-    for (var i = 0; i < steps; i++) {
-      this.takeStep()
+          return accum
+        }, {})
+
+      return Object.keys(visitCounts)
+        .filter((position) => {
+          return visitCounts[position] > 1
+        })
+        .map(JSON.parse)
     }
   }
 }
 
-for (var i = 0; i < steps.length; i++) {
-  let step = steps[i]
-
-  let direction = step.substring(0,1)
-  let stepCount = step.substring(1)
-
-  santa.turn(direction)
-  santa.takeSteps(parseInt(stepCount))
+function doNTimes(n, f) {
+  Array.from(Array(n)).forEach(f)
 }
 
-const distance = Math.abs(santa.position().x) + Math.abs(santa.position().y)
-console.log(distance)
+function distanceFromOrigin(position) {
+  return Math.abs(position.x) + Math.abs(position.y)
+}
 
+const instructionsText = fs.readFileSync('input.txt', 'utf8')
+const instructions = instructionsText.split(', ')
+  .map((instruction) => {
+    return {
+      direction: instruction.substring(0, 1),
+      stepCount: parseInt(instruction.substring(1)),
+    }
+  })
+
+var santa = new Santa()
+
+instructions.forEach((instruction) => {
+  santa.turn(instruction.direction)
+  doNTimes(instruction.stepCount, santa.moveForward.bind(santa))
+})
+
+const totalDistance = distanceFromOrigin(santa.position())
+const firstRevisit = santa.revisits()[0]
+const firstRevisitDistance = distanceFromOrigin(firstRevisit)
+
+console.log(`Blocks from start: ${totalDistance}`)
+console.log('First revisited:', firstRevisit)
+console.log(`First revisited blocks from start: ${firstRevisitDistance}`)
